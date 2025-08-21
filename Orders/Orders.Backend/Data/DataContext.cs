038106c9af5f0efa32e22fd36b0666efb7bb16d5
Jeffry Kamsteeg Rodiño <jeffrykamsteeg@hotmail.com>
2025-08-21 14:40:06 -0500

    Add contries entity

---
+++ Orders/Orders.Backend/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Contry> Contries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Contry>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
