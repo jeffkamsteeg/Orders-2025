2f44eba09ce0ce8ea563522f0888de9819694d78
Jeffry Kamsteeg Rodiño <jeffrykamsteeg@hotmail.com>
2025-09-17 19:42:39 -0500

    Add DB Seeder

---
+++ Orders/Orders.Backend/Data/SeedDb.cs
@@ -0,0 +1,43 @@
+﻿using Orders.Shared.Entities;
+
+namespace Orders.Backend.Data;
+
+public class SeedDb //Clase para inicializar la base de datos con datos predeterminados.
+                    // Para ello se crea una instancia de esta clase en Program.cs
+                    // y se llama al método SeedAsync().
+{
+    private readonly DataContext _context; //Contexto de la base de datos.
+
+    public SeedDb(DataContext context) //Constructor que recibe el contexto de la base de datos, inyección de dependencia.
+    {
+        _context = context; //Asignar el contexto.
+    }
+
+    public async Task SeedAsync() //Método para inicializar la base de datos.
+    {
+        await _context.Database.EnsureCreatedAsync(); //Crear la base de datos si no existe.
+        await CheckContriesAsync(); //Verificar si hay países.
+        await CheckCategoriesAsync(); //Verificar si hay categorías.
+    }
+
+    private async Task CheckCategoriesAsync() //Verificar si hay categorías.
+    {
+        if (!_context.Categories.Any()) //Si no hay categorías..
+        {
+            _context.Categories.Add(new Category { Name = "Calzado" }); //Agregar categoría "Calzado".
+            _context.Categories.Add(new Category { Name = "Tecnología" }); //Agregar categoría "Tecnología".
+            await _context.SaveChangesAsync(); //Guardar los cambios en la base de datos.
+        }
+    }
+
+    private async Task CheckContriesAsync() //Verificar si hay países.
+    {
+        if (!_context.Countries.Any()) //Si no hay países..
+        {
+            _context.Countries.Add(new Country { Name = "Colombia" }); //Agregar país "Colombia".
+            _context.Countries.Add(new Country { Name = "Estados Unidos" }); //Agregar país "Estados Unidos".
+            _context.Countries.Add(new Country { Name = "México" }); //Agregar país "México".
+            await _context.SaveChangesAsync(); //Guardar los cambios en la base de datos.
+        }
+    }
+}
--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -13,12 +13,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection")); // Configuración de la cadena de conexión a SQL Server desde appsettings.json
+builder.Services.AddTransient<SeedDb>(); // Inyección de dependencia para la clase SeedDb
 
 builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 var app = builder.Build();
+
+SeedData(app); // Llamada al método para inicializar la base de datos
+
+void SeedData(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>(); // Obtener el servicio de creación de ámbitos, los cuales
+    //permiten gestionar el ciclo de vida de los servicios para que se ha configurado un ámbito (scoped).
+
+    using var scope = scopedFactory!.CreateScope(); // Crear un nuevo ámbito de servicio para la inicialización de datos.
+    var service = scope.ServiceProvider.GetService<SeedDb>(); // Obtener una instancia de la clase SeedDb desde el proveedor de servicios del ámbito.
+    service!.SeedAsync().Wait(); // Llamar al método SeedAsync para inicializar la base de datos y esperar a que complete.
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
